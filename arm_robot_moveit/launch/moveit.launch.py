5bd14b2477ee74e3ed6161d5fe07432c56297fa2
AdithyaDamarla <adithyaprakash12@gmail.com>
2023-09-07 16:02:30 +0200

    minor fixes

--- arm_robot_moveit/launch/moveit.launch.py
+++ arm_robot_moveit/launch/moveit.launch.py
@@ -10,13 +10,14 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    is_sim = LaunchConfiguration("is_sim")
+
     is_sim_arg = DeclareLaunchArgument(
         "is_sim",
         default_value="True",
         
     )
-
-    is_sim = LaunchConfiguration("is_sim")
 
     moveit_config = (MoveItConfigsBuilder("arm_robot", package_name="arm_robot_moveit")
                      .robot_description(file_path=os.path.join(get_package_share_directory("robot_description"), "urdf", "robot_des.urdf.xacro"))
